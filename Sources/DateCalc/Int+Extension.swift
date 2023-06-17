41b955a87585786456120b762328f660e2a676bc
hottin <hotterdragon@gmail.com>
2023-06-17 22:45:33 +0900

    add: Extensionの追加

---
+++ Sources/DateCalc/Int+Extension.swift
@@ -0,0 +1,65 @@
+//
+// Created by 堀田竜也 on 2023/06/17.
+//
+
+import Foundation
+
+fileprivate let components: [Calendar.Component] = [
+    .nanosecond,
+    .second,
+    .minute,
+    .hour,
+    .day,
+    .month,
+    .year,
+    .quarter
+]
+
+public extension Int {
+
+    fileprivate func toDateComponents(_ type: Calendar.Component) -> DateComponents {
+        var dateComponents = DateComponents()
+        components.forEach {
+            dateComponents.setValue(0, for: $0)
+        }
+        dateComponents.setValue(self, for: type)
+        dateComponents.setValue(0, for: .era)
+        return dateComponents
+    }
+
+    var nanoseconds: DateComponents {
+        toDateComponents(.nanosecond)
+    }
+
+    var seconds: DateComponents {
+        toDateComponents(.second)
+    }
+
+    var minutes: DateComponents {
+        toDateComponents(.minute)
+    }
+
+    var hours: DateComponents {
+        toDateComponents(.hour)
+    }
+
+    var days: DateComponents {
+        toDateComponents(.day)
+    }
+
+    var weeks: DateComponents {
+        toDateComponents(.weekOfYear)
+    }
+
+    var months: DateComponents {
+        toDateComponents(.month)
+    }
+
+    var years: DateComponents {
+        toDateComponents(.year)
+    }
+
+    var quarters: DateComponents {
+        toDateComponents(.quarter)
+    }
+}
